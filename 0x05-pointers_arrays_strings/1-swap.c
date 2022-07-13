e36adfc0b04941393f69e0cde3973f954e377bd1
eddcode123 <edwinmutugi29@gmail.com>
2022-07-13 15:46:53 +0300

    adding file

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,3 +1,14 @@
 #include "main.h"
 /**
- *
+ * swap_int: swaps integer values from two variables
+ * @a: is a variable storing 1st integer
+ * @b: 2nd variable storing an integer
+ * return: we are not returning anything since is a void
+ */
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
