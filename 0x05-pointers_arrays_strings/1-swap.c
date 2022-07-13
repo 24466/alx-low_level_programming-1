5efe00ecbb210b4ecb7e93e1ec915c29c6ac04f4
eddcode123 <edwinmutugi29@gmail.com>
2022-07-13 15:49:28 +0300

    adding comment discription on function

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * swap_int: swaps integer values from two variables
+ * swap_int-swaps integer values from two variables
  * @a: is a variable storing 1st integer
  * @b: 2nd variable storing an integer
  * return: we are not returning anything since is a void
