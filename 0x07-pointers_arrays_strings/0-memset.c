a71e85d9c2393f8662d34b7a931e54cccc0cbd78
eddcode123 <edwinmutugi29@gmail.com>
2022-07-22 10:42:13 +0300

    fixfile

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 /**
- * _memset(): fuction that fills n bytes of memory area pointed to by s with *s with constant byte b
- * char *s: is a charachter pointer varible
- * char b: is a character variable 
- * int n : is a interger variable
+ *_memset - fuction that fills n with b pointed to by s
+ * @s: is a charachter pointer varible
+ * @b: is a character variable
+ * @n : is a interger variable
+ * Return: pointer to dest(destination)
  */
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
-	n = b;
-	*s = &n;
-	return s;
+	unsigned int i;
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
 }
