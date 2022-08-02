86e11743ba3a21ec8298e22077c062be49e06876
eddcode123 <edwinmutugi29@gmail.com>
2022-08-02 16:55:11 +0300

    new update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 48; i < 58; i++)
+	for (i = 48; i < 57 ; i++)
 	{
-		for (j = 48; j < 58; j++)
+		for (j = 49; j < 58; j++)
 		{
 			putchar(i);
 			putchar(j);
