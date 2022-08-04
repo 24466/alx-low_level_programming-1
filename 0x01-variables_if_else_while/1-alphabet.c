b139c0f720825bfa60634c0d6379f70a148fad16
eddcode123 <edwinmutugi29@gmail.com>
2022-08-04 16:29:59 +0300

    updatefile

---
+++ 0x01-variables_if_else_while/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - print lowercase alphabet
+ *
+ */
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 97; a < 123; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
