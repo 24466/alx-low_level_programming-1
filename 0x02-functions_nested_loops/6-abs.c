5e79951c1926f9f69726acceda40b472c5ac0ec7
eddcode123 <edwinmutugi29@gmail.com>
2022-08-05 14:55:41 +0300

    updatefile

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,10 +3,10 @@
 #include <stdlib.h>
 /**
  * _abs - computes the absolute value of an integer
- * @int: integer value
+ * @a: integer value
  * Return: always 0 (sucess)
  */
-int _abs(int)
+int _abs(int a)
 {
 	int a, b;
 
