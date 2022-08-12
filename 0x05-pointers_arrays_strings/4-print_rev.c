c73809d789dad62ce900991a296d962a3faf9347
eddcode123 <edwinmutugi29@gmail.com>
2022-08-12 12:22:46 +0300

    updatefile

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,19 +8,8 @@
  */
 void print_rev(char *s)
 {
-	int i, left, right, len, temp;
+	int a = strlen(s);
 
-	left = 0;
-	right = len - 1;
-	len = strlen(s);
-	for (i = left; i < right; i++)
-	{
-		s[i] = temp;
-		s[i] = s[right];
-		s[right] = temp;
-
-		right--;
-	}
 	printf("%s\n", *&s);
 }
 
