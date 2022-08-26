146aad8545176f08a8b898d368685df5622c509a
eddcode123 <edwinmutugi29@gmail.com>
2022-08-26 11:59:33 +0300

    updatefile

---
+++ 0x02-functions_nested_loops/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swaps the values of two integers
+ * @a: input value ingeter pointer
+ * @b: input value ingeter pointer
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int *swap;
+
+	swap = &a;
+	a = b;
+	b = swap;
+}
