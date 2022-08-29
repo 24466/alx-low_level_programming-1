354bdb4ad75134bda3d4f67dce95f4615539bb3c
eddcode123 <edwinmutugi29@gmail.com>
2022-08-29 13:58:18 +0300

    updatefile

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
 /**
  * print_rev - prints a sting in revrese and newline
  * @s: string input value
@@ -10,16 +9,13 @@ void print_rev(char *s)
 {
 	int i;
 	char temp;
-	int len = strlen(s);
+	int len = _strlen(s);
 
 	for (i = 0; i <= len / 2; i++)
 	{
-		if (i != '\0')
-		{
-			temp = s[i];
-			s[i] = s[len - 1 - i];
-			s[len - 1 - i] = temp;
-		}
+		temp = s[i];
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] = temp;
 	}
 	printf("%s", s);
 	printf("\n");
