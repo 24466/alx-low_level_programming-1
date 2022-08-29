c4fc430fd1a5b12f32968f5b2ea7a6a80fb4d89a
eddcode123 <edwinmutugi29@gmail.com>
2022-08-29 13:27:16 +0300

    updatefile

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-int _strlen(char *s);
+#include <string.h>
 /**
  * print_rev - prints a sting in revrese and newline
  * @s: string input value
@@ -10,7 +10,7 @@ void print_rev(char *s)
 {
 	int i;
 	char temp;
-	int len = _strlen(s);
+	int len = strlen(s);
 
 	for (i = 0; i < len / 2; i++)
 	{
