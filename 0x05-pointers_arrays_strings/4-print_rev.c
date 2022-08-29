d3bdcbccb583e81033f5977573aba6a7f30bfd6a
eddcode123 <edwinmutugi29@gmail.com>
2022-08-29 15:07:15 +0300

    updatefile

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,7 +8,16 @@
  */
 void print_rev(char *s)
 {
-	strrev(s);
+	int i;
+	int len = strlen(s);
+	char temp;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] = temp;
+	}
 	printf("%s", s);
 	printf("\n");
 }
